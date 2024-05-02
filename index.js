e1e262c89fd17d73d0b5a75abdd1a8789338645a
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2024-05-02 11:55:11 +0530

    backend done

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+// import { fileURLToPath } from 'url';
+// import path ,{ dirname } from 'path';
+
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
+
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+
+try{
+    mongoose.connect(URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log("Connected to MongoDB");
+} catch (error) {
+    console.log("Error: ", error);
+}
+
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
+
+// Get the directory name of the current module
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// app.get("/", (req, res) => {
+//     app.use(express.static(path.resolve(__dirname, "Frontend", "dist")));
+//     res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+// });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
